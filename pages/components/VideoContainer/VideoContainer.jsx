230b507d342244e509ff240863753b6d4d5b69ce
patrick-hutchinson <hutchinsonpatrick@icloud.com>
2025-03-10 16:45:58 +0100

    Update VideoContainer.jsx

--- pages/components/VideoContainer/VideoContainer.jsx
+++ pages/components/VideoContainer/VideoContainer.jsx
@@ -7,104 +7,104 @@ import * as THREE from "three";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 export default function VideoContainer({ medium }) {
-  const imageWrapperRef = useRef(null);
+  //   const imageWrapperRef = useRef(null);
 
-  // Three JS Scene Creation
-  const sceneRenderedRef = useRef(false);
-  const mountRef = useRef(null);
-  useEffect(() => {
-    if (sceneRenderedRef.current) return;
-    sceneRenderedRef.current = true;
+  //   // Three JS Scene Creation
+  //   const sceneRenderedRef = useRef(false);
+  //   const mountRef = useRef(null);
+  //   useEffect(() => {
+  //     if (sceneRenderedRef.current) return;
+  //     sceneRenderedRef.current = true;
 
-    // *** DEFINE SCENE
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color("red");
+  //     // *** DEFINE SCENE
+  //     const scene = new THREE.Scene();
+  //     scene.background = new THREE.Color("red");
 
-    const width = imageWrapperRef.current.getBoundingClientRect().width;
-    const height = imageWrapperRef.current.getBoundingClientRect().height;
-    console.log("Scene dimensions:", width, height);
+  //     const width = imageWrapperRef.current.getBoundingClientRect().width;
+  //     const height = imageWrapperRef.current.getBoundingClientRect().height;
+  //     console.log("Scene dimensions:", width, height);
 
-    const camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 1, 1000);
-    camera.position.z = 100;
+  //     const camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 1, 1000);
+  //     camera.position.z = 100;
 
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(width, height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Prevent extreme jumps
-    mountRef.current.appendChild(renderer.domElement);
+  //     const renderer = new THREE.WebGLRenderer({ alpha: true });
+  //     renderer.setSize(width, height);
+  //     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Prevent extreme jumps
+  //     mountRef.current.appendChild(renderer.domElement);
 
-    // *** LOAD IMAGE
-    const texture = new THREE.TextureLoader().load(medium.url, () => {
-      console.log("Image loaded");
-    });
+  //     // *** LOAD IMAGE
+  //     const texture = new THREE.TextureLoader().load(medium.url, () => {
+  //       console.log("Image loaded");
+  //     });
 
-    const geometry = new THREE.PlaneGeometry(width, height);
+  //     const geometry = new THREE.PlaneGeometry(width, height);
 
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        imageTexture: { value: texture },
-        threshold: { value: 1 },
-      },
-      vertexShader: `
-                                varying vec2 vUv;
-                                void main() {
-                                    vUv = uv;
-                                    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                                }
-                            `,
-      fragmentShader: `
-                                uniform sampler2D imageTexture;
-                                uniform float threshold;
-                                varying vec2 vUv;
-                                void main() {
-                                    vec4 texel = texture2D(imageTexture, vUv);
-                                    float brightness = dot(texel.rgb, vec3(0.299, 0.587, 0.114));
-                                    if (brightness > threshold) {
-                                        gl_FragColor = vec4(texel.rgb, 1.0);
-                                    } else {
-                                        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-                                    }
-                                }
-                            `,
-    });
+  //     const material = new THREE.ShaderMaterial({
+  //       uniforms: {
+  //         imageTexture: { value: texture },
+  //         threshold: { value: 1 },
+  //       },
+  //       vertexShader: `
+  //                                 varying vec2 vUv;
+  //                                 void main() {
+  //                                     vUv = uv;
+  //                                     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  //                                 }
+  //                             `,
+  //       fragmentShader: `
+  //                                 uniform sampler2D imageTexture;
+  //                                 uniform float threshold;
+  //                                 varying vec2 vUv;
+  //                                 void main() {
+  //                                     vec4 texel = texture2D(imageTexture, vUv);
+  //                                     float brightness = dot(texel.rgb, vec3(0.299, 0.587, 0.114));
+  //                                     if (brightness > threshold) {
+  //                                         gl_FragColor = vec4(texel.rgb, 1.0);
+  //                                     } else {
+  //                                         gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+  //                                     }
+  //                                 }
+  //                             `,
+  //     });
 
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.userData.aspectRatio = height / width;
-    scene.add(mesh);
+  //     const mesh = new THREE.Mesh(geometry, material);
+  //     mesh.userData.aspectRatio = height / width;
+  //     scene.add(mesh);
 
-    const anim = gsap.to(material.uniforms.threshold, {
-      value: -1,
-      duration: 2.5,
-      paused: true,
-    });
+  //     const anim = gsap.to(material.uniforms.threshold, {
+  //       value: -1,
+  //       duration: 2.5,
+  //       paused: true,
+  //     });
 
-    ScrollTrigger.create({
-      trigger: imageWrapperRef.current,
-      start: "top 20%", // Trigger when the top of the element is 80% from the top of the viewport
-      onEnter: () => anim.play(),
-      onEnterBack: () => anim.play(),
-      onLeave: () => {
-        material.uniforms.threshold.value = 1;
-        anim.progress(0).pause();
-      },
-      onLeaveBack: () => {
-        material.uniforms.threshold.value = 1;
-        anim.progress(0).pause();
-      },
-      once: false,
-    });
+  //     ScrollTrigger.create({
+  //       trigger: imageWrapperRef.current,
+  //       start: "top 20%", // Trigger when the top of the element is 80% from the top of the viewport
+  //       onEnter: () => anim.play(),
+  //       onEnterBack: () => anim.play(),
+  //       onLeave: () => {
+  //         material.uniforms.threshold.value = 1;
+  //         anim.progress(0).pause();
+  //       },
+  //       onLeaveBack: () => {
+  //         material.uniforms.threshold.value = 1;
+  //         anim.progress(0).pause();
+  //       },
+  //       once: false,
+  //     });
 
-    // *** RENDER
-    const animate = () => {
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
+  //     // *** RENDER
+  //     const animate = () => {
+  //       renderer.render(scene, camera);
+  //       requestAnimationFrame(animate);
+  //     };
 
-    animate();
-    // ***
-  }, []);
+  //     animate();
+  //     // ***
+  //   }, []);
 
   return (
     <div
